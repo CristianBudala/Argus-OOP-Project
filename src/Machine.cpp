c1a680cb3f804ecb58b9df791ac43c8a9a089f8d
Cristian Budala <cristian.budala1@gmail.com>
2026-04-01 15:41:17 +0300

    implemented class Machine, started class Alert

---
+++ src/Machine.cpp
@@ -0,0 +1,25 @@
+#include "../include/Machine.h"
+
+Machine::Machine(const std::string& hostname, const std::string& ipAddress)
+    : hostname(hostname), ipAddress(ipAddress) {}
+
+Machine::~Machine() {
+    for (auto mt : metrics)
+        delete mt;
+}
+
+void Machine::addMetric(Metric* m) { metrics.push_back(m); }
+
+void Machine::collectAll() { 
+    for (auto mt : metrics)
+        mt->collect();
+}
+
+void Machine::displayAll() const {
+    for (auto mt : metrics)
+        std::cout << *mt << std::endl;
+}
+
+const std::string& Machine::getHostname() const { return hostname; }
+
+const std::string& Machine::getIpAddress() const { return ipAddress; }
